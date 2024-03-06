9cd43568ebc11a30b75e64092363d3183314e363
David <saumet.david@orange.fr>
2024-03-06 15:02:54 +0100

    add bouton

--- src/views/MonCv.vue
+++ src/views/MonCv.vue
@@ -4,7 +4,30 @@
        <p>Celui-ci a été créé le 07 janvier 2024.</p>
        <p>J'ai utilisé l'application VScode afin de rédiger mes pages HTML et CSS.</p>
        <p>L'application Github a été nécessaire afin de créer un repository consultable à https://github.com/DS30190/Devoir-CV</p>
-       <p>Mon CV est consultable à</p>
-       <button class="close" type="button" >Close</button>
+       <p>Mon CV est consultable à https://cv.DS30190.fr/</p>
+       <img src="@/assets/images/Photocv.jpg" alt="Mon cv" width="300" height="450" >
+       <br />
+       <button class="close" @click="fermer">Close</button>
+       
     </div>
-</template> 
+</template>
+
+<script>
+export default {
+  name: 'MonCv',
+  methods: {
+  fermer() {
+    window.location.href = './projects'; 
+  }
+}
+  }
+
+</script>
+
+<style>
+* {
+  text-align: center;
+}
+</style>
+
+
